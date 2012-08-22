1028c662fc9604854a7d3473789ee136e829bebe
John Bickar <jbickar@stanford.edu>
2012-08-22 11:59:06 -0700

    Replaced hard-coded HTML image paths with PHP references First implementation of l().

--- templates/block--stanford-sites-helper--firststeps.tpl.php
+++ templates/block--stanford-sites-helper--firststeps.tpl.php
@@ -26,13 +26,14 @@ $images = drupal_get_path('module', 'stanford_sites_helper') . '/images/';
           <legend><img src="<?php print $images ?>plant.png" alt="plant"> <span class="fieldset-legend">Change your site's name and information</span></legend>
           <div class="fieldset-wrapper">
             <p class="intro">You can rename your site, add a slogan, or set an existing page to be the homepage of your site by visiting the site information page.</p>
+            <p> <?php l((t"Edit Your Site's Information"), 'admin/config/system/site-information', array('class' => array("btn"))); ?> </p>
             <p><a href="#" class="btn">Edit Your Site's Information</a></p>
           </div>
         </fieldset>
       </li>
       <li>
         <fieldset class="field-group-fieldset collapsible collapsed">
-          <legend><img src="images/pencil.png" alt="pencil"> <span class="fieldset-legend">Add content to your homepage</span></legend>
+          <legend><img src="<?php print $images ?>pencil.png" alt="pencil"> <span class="fieldset-legend">Add content to your homepage</span></legend>
           <div class="fieldset-wrapper">
             <p class="intro">Use your homepage to add a static welcome or introduction to your site. If you want a blog as part of your site (or on your site's homepage), visit the Modules page to enable the Stanford Blog feature.</p>
             <p><a href="#" class="btn">Edit Your Homepage</a></p>
@@ -42,7 +43,7 @@ $images = drupal_get_path('module', 'stanford_sites_helper') . '/images/';
       </li>
       <li>
         <fieldset class="field-group-fieldset collapsible collapsed">
-          <legend><img src="images/plus.png" alt="plus"> <span class="fieldset-legend">Create a new page on your site</span></legend>
+          <legend><img src="<?php print $images ?>plus.png" alt="plus"> <span class="fieldset-legend">Create a new page on your site</span></legend>
           <div class="fieldset-wrapper">
             <p class="intro">Do you want an &quot;about&quot; or other page  on your site? When creating new pages, you can directly add them to the Main Menu navigation in the Menu Settings section of the form.</p>
             <p><a href="#" class="btn">Create a New Page</a></p>
@@ -57,7 +58,7 @@ $images = drupal_get_path('module', 'stanford_sites_helper') . '/images/';
     <ol>
       <li>
         <fieldset class="field-group-fieldset collapsible collapsed">
-          <legend><img src="images/menu.png" alt="menu"> <span class="fieldset-legend">Edit your  navigation menus</span></legend>
+          <legend><img src="<?php print $images ?>menu.png" alt="menu"> <span class="fieldset-legend">Edit your  navigation menus</span></legend>
           <div class="fieldset-wrapper">
             <p class="intro">Drupal comes pre-packaged with a Main menu navigation  to which you can add your existing pages. First create new pages, and then add them to your menu. You can create a new menu and add items to it by visiting the Menus page. Move your menus around using the Blocks page.</p>
             <p><a href="#" class="btn">Edit Your Main Menu</a> <a href="#" class="btn">Create a New Menu</a></p>
@@ -67,7 +68,7 @@ $images = drupal_get_path('module', 'stanford_sites_helper') . '/images/';
       </li>
       <li>
         <fieldset class="field-group-fieldset collapsible collapsed">
-          <legend><img src="images/gear.png" alt="gear"> <span class="fieldset-legend">Add functionality and features</span></legend>
+          <legend><img src="<?php print $images ?>gear.png" alt="gear"> <span class="fieldset-legend">Add functionality and features</span></legend>
           <div class="fieldset-wrapper">
             <p class="intro">Want to add a blog to your site? Looking to do more with your site? Enable functionality on the Modules page, but first you might want to learn what the available modules do and why you might want to enable them.</p>
             <p><a href="#" class="btn">Visit Your Modules</a></p>
@@ -77,7 +78,7 @@ $images = drupal_get_path('module', 'stanford_sites_helper') . '/images/';
       </li>
       <li>
         <fieldset class="field-group-fieldset collapsible collapsed">
-          <legend><img src="images/brush.png" alt="brush"> <span class="fieldset-legend">Edit the look and feel of your site</span></legend>
+          <legend><img src="<?php print $images ?>brush.png" alt="brush"> <span class="fieldset-legend">Edit the look and feel of your site</span></legend>
           <div class="fieldset-wrapper">
             <p class="intro">Your site comes pre-packaged with the Stanford Basic theme. Edit the settings for Stanford Basic to add a background image and change the color scheme. If you're looking to do more custom CSS for your site, use the CSS Injector module.</p>
             <p><a href="#" class="btn">Edit My Theme's Settings</a></p>
