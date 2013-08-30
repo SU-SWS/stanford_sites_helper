ecacdbfbb19e4f9b1c6a5fd3b81e13aff36fd54f
John Bickar <jbickar@stanford.edu>
2013-08-30 15:43:08 -0700

    Updating logic for welcome text in First Steps block

--- templates/firststeps.tpl.php
+++ templates/firststeps.tpl.php
@@ -6,7 +6,16 @@
 /*
  * Variables
  */
-$user_login_text = l(t('Log in with your SUNetID.'), drupal_get_path('module', 'webauth') . '/login');
+$user_login_text = l(t('Log in with your SUNetID.'), 'sites/default/webauth/login');
+$user_login_block = '<p>' . $user_login_text . '</p>';
+$password_reset = l(t('password reset form.'), 'user/password');
+// check if WMD is set to allow local Drupal logins.
+// if so, display the login text to have the user log in as user 1
+if (variable_get('webauth_allow_local') != 1) {
+  $user_login_block = "<p>If you haven't already, you should log in via the form to the left.</p>\n";
+  $user_login_block .= "<p>Your user name is <strong>admin</strong> and you created a password when you set up the site.</p>\n";
+  $user_login_block .= "<p>If you cannot remember your password, you can reset it by entering <strong>&quot;admin&quot;</strong> as the username in the " . $password_reset . "</p>\n";
+}
 $images = drupal_get_path('module', 'stanford_sites_helper') . '/images/';
 $stanford_sites_helper_node = variable_get('stanford_sites_helper_node');
 $edit_homepage = l(t("Edit Your Homepage Content"), 'node/' . $stanford_sites_helper_node . '/edit', array('attributes' => array('class' => array('btn'))));
@@ -36,7 +45,7 @@ drupal_add_css($path);
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 <h2>Welcome to Your Stanford Sites Website</h2>
-  <p><?php if(user_is_anonymous()) { print $user_login_text; }?></p>
+  <?php if(user_is_anonymous()) { print $user_login_block; }?>
   <p>Once you have logged in, you will be able to remove this text from your homepage.</p>
 
 <hr />
